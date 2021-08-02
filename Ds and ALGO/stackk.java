e25361a9b75530c34cde707b28e5dfefdba346b2
Kanishq <79330066+Kanishq10@users.noreply.github.com>
2021-08-02 22:16:18 +0530

    commit

---
+++ Ds and ALGO/stackk.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+class StackList{
+    int info;
+    StackList next,top=null;
+    StackList(){
+        next=null;
+    }
+    StackList(int info){
+        this.info=info;
+        next=null;
+    }
+
+    StackList push(int info){
+        StackList n=new StackList(info);
+        if(top==null){
+            top=n;
+            return top;
+        }
+        else{
+            n.next=top;
+            top=n;
+            return top;
+        }
+    }
+
+    StackList pop(){
+        if(top==null){
+            System.out.println("underflow");
+            return top;
+        }
+        else{
+            StackList temp=top;
+            top=top.next;
+            temp.next=null;
+            return temp;
+        }
+    }
+
+    int peek(){
+        if(top==null){
+            System.out.println("Underflow");
+            return -1;
+        }
+        else{
+            System.out.println(top.info);
+            return top.info;
+        }
+    }
+}
+
+class StackArray{
+    int arr[]=new int[100];
+    int top=-1;
+    void push(int info){
+        if(top==arr.length-1){
+            System.out.println("OverFlow");
+            return;
+        }
+        top+=1;
+        arr[top]=info;
+    }
+
+    void pop(){
+        if(top==-1){
+            System.out.println("underflow");
+            return;
+        }
+        top=top-1;
+    }
+
+    int peek(){
+        if(top==-1){
+            System.out.println("UnderFlow");
+            return -1;
+        }
+        System.out.println(arr[top]);
+        return arr[top];
+    }
+}
+
+class stackk {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter Choice \n1.For Linked List Stack\n2.For Array Stack ");
+        int ch=s.nextInt();
+        switch(ch){
+            case 1:
+            StackList x=new StackList();
+            x.push(2);
+            x.push(3);
+            x.peek();
+            x.pop();
+            x.peek();   
+            break;
+            case 2:
+            StackArray a=new StackArray();
+            a.push(12);
+            a.peek();
+            a.push(24);
+            a.push(45);
+            a.peek();
+            a.pop();
+            a.peek();
+            a.pop();
+            a.peek();
+            break;
+            default:System.out.println("Wrong choice");                     
+        }
+        s.close(); 
+    }    
+}
