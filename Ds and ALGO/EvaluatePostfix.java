735039d8bfc05c98370843472e4a7388316e76ad
Kanishq <79330066+Kanishq10@users.noreply.github.com>
2021-08-03 23:16:34 +0530

    prefix postfix programs

---
+++ Ds and ALGO/EvaluatePostfix.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+class EvaluatePostfix {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String exp=s.nextLine();
+        Stack<Integer> st=new Stack<>();
+        for (int i = 0; i < exp.length(); i++) {
+            char c=exp.charAt(i);
+            if(Character.isDigit(c)){
+                st.push(c-'0');
+            }    
+            else{
+                int a=st.pop();
+                int b=st.pop();
+                switch(c){
+                    case '+':st.push(b+a);
+                            break;
+                    case '-':st.push(b-a);
+                             break;
+                    case '*':st.push(b*a);
+                             break;
+                    case '/':st.push(b/a);
+                            break;
+                    case '^':st.push(b^a);
+                            break;
+                    case '%':st.push(b%a);
+                            break;
+                }
+            }        
+        }
+        int answer=st.pop();
+        System.out.println(answer);
+        s.close();
+    }   
+}
---
+++ Ds and ALGO/EvaluatePrefix.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Stack;
+class EvaluatePrefix {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String exp=in.nextLine();
+        Stack<Integer> st=new Stack<>();
+        for(int i=exp.length()-1;i>=0;i--){
+            char c=exp.charAt(i);
+            if(Character.isDigit(c)){
+                st.push(c-'0');
+            }
+            else{
+                int a=st.pop();
+                int b=st.pop();
+                switch (c) {
+                    case '+':
+                        st.push(a+b);
+                        break;
+                    case '-':
+                    st.push(a-b);
+                    break;
+                    case '*':
+                    st.push(a*b);
+                    break;
+                    case '/':
+                    st.push(a/b);
+                    case '^':
+                    st.push(a^b);
+                    break;
+                    case '%':
+                    st.push(a%b);
+                }
+            }
+        }
+        int result=st.pop();
+        System.out.println(result);
+        in.close();
+    }    
+}
