c24f2e8bd8c3f7084526417406294b592e01ea10
Kanishq <79330066+Kanishq10@users.noreply.github.com>
2021-08-14 01:16:37 +0530

    adding programs

---
+++ Ds and ALGO/FindMaxValueWithRotations.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//Given an array, only rotation operation is allowed on array. We can rotate the array as many times as we want. Return the maximum possible summation of i*arr[i].
+class FindMaxValueWithRotations{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++){
+            
+        }
+    }
+}
