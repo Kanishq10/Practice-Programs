1db0bc417bb47e2c0b8f2cb856250dfff68156af
Kanishq <79330066+Kanishq10@users.noreply.github.com>
2021-08-24 01:11:45 +0530

    Create PrintArrayNtimeswithModulus.java

---
+++ java.files/PrintArrayNtimeswithModulus.java
@@ -0,0 +1,10 @@
+class PrintArrayNtimeswithModulus {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        int n=arr.length;
+        int t=5;
+        for (int i = 0; i < t*n; i++) {
+            System.out.print(arr[i%n]+" ");            
+        }
+    }    
+}
