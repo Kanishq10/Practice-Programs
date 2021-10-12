e062d8d0d3c7ac109a3c1485bb0f17aae5a0f6f4
Kanishq <79330066+Kanishq10@users.noreply.github.com>
2021-10-13 00:08:14 +0530

    Update QueuePriorityArray.java

--- Ds and ALGO/QueuePriorityArray.java
+++ Ds and ALGO/QueuePriorityArray.java
@@ -14,7 +14,12 @@ class QPA{
 
     void insert(int data,int priority){
         int i=0;
-        while((pno[i]!=priority) && (front[i]==0 && rear[i]==19 || front[i]==rear[i]-1) && (pno[i]!=-1)){
+        while(true){
+            if(pno[i]==priority || pno[i]==-1){
+                if(!(front[i]==0 && rear[i]==19 || front[i]==rear[i]-1)){
+                        break;
+                }
+            }
             i++;
         }
         pno[i]=priority;
@@ -22,7 +27,7 @@ class QPA{
             front[i]=0;
             rear[i]=0;
         }
-        else if(rear[i]==19 && front[i]!=0){
+        else if(rear[i]==19){
             rear[i]=0;
         }
         else{
@@ -31,32 +36,44 @@ class QPA{
         arr[i][rear[i]]=data;
     }
 
-    void delete(){
-        int i=0;
-        while(((front[i]==-1 && rear[i]==-1) || (front[i]==rear[i]-1)) && pno[i]!=-1){
-            i++;
-        }
-        if(front[i]==rear[i]){
-            front[i]=-1;
-            rear[i]=-1;
-        }
-        else if(front[i]==19){
-            front[i]=0;
-        }
-        else{
-            front[i]++;
-        }
-    }
+    // void delete(){
+    //     int i=0;
+    //     // while(true){
+    //     //     if(pno[i]==-1 || (front[i]!=-1 && front[i]!=-1)){
+    //     //         break;
+    //     //     }
+    //     //     i++;
+    //     // }
+    //     int min=minimum(){
+
+    //     }
+    //     while(pno[i]!=-1){
+    //         if()
+    //     }
+    //     if(front[i]==rear[i]){
+    //         front[i]=-1;
+    //         rear[i]=-1;
+    //     }
+    //     else if(front[i]==19){
+    //         front[i]=0;
+    //     }
+    //     else{
+    //         front[i]++;
+    //     }
+    // }
 
     void delete(int priority){
         int i=0;
-        while((front[i]==-1 && rear[i]==-1) || pno[i]!=priority){
-            i++;
-            if(pno[i]==-1){
+        while(true){
+            if(pno[i]==-1 || pno[i]==priority){
                 break;
             }
+            i++;
         }
-        if(front[i]==rear[i]){
+        if(front[i]==-1 && rear[i]==-1){
+            return;
+        }
+        else if(front[i]==rear[i]){
             front[i]=-1;
             rear[i]=-1;
         }
@@ -80,6 +97,11 @@ class QPA{
                 System.out.print(arr[i][j]+" ");
             }
         }
+        else{
+            for(int j=front[i];j<=rear[i];j++){
+                System.out.print(arr[i][j]+" ");
+            }
+        }
     }
 
     void print(int priority){
@@ -88,6 +110,7 @@ class QPA{
             if(pno[i]==priority){
                 display(i);
             }
+            i++;
         }
         System.out.println();
     }
@@ -99,5 +122,26 @@ class QueuePriorityArray {
         x.insert(232, 2);
         x.insert(23, 2);
         x.print(2);
+        x.insert(45, 6);
+        x.insert(23, 8);
+        x.insert(12, 8);
+        x.insert(13, 8);
+        x.insert(15, 8);
+        x.insert(17, 8);
+        x.insert(112, 8);
+        x.insert(32, 7);
+        x.print(2);
+        x.print(6);
+        x.print(7);
+        x.print(8);
+        x.delete(2);
+        x.delete(6);
+        x.delete(7);
+        x.delete(8);
+        x.delete(8);
+        x.print(2);
+        x.print(6);
+        x.print(7);
+        x.print(8);
     }
 }
