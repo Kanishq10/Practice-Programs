ec9d93087a41ddd8259cd4957a522f2b5f646403
Kanishq10 <kanishq301@gmail.com>
2021-11-09 10:49:39 +0530

    Create DijsktraALGO.java

---
+++ Ds and ALGO/DijsktraALGO.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+class DIJ{
+    Scanner in=new Scanner(System.in);
+    int arr[];
+    int mat[][];
+    DIJ(){
+        arr=new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i]=in.nextInt();
+        }
+        mat=new int[5][5];
+    }
+    DIJ(int n){
+        arr=new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=in.nextInt();
+        }
+        mat=new int[n][n];
+    }
+    
+    void insert(int v1,int v2,int d){
+        int i=-1,j=-1;
+        for(int f=0;f<arr.length;f++){
+            if(arr[f]==v1){
+                i=f;
+            }
+            if(arr[f]==v2){
+                j=f;
+            }
+        }
+        if(i!=-1 && j!=-1){
+            mat[i][j]=d;  
+            mat[j][i]=d;     //we are overiding the weight
+        }
+    }
+
+    void dijisktra(int n,int s){
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=Integer.MAX_VALUE;
+        }
+        a[s]=0;
+        
+    }
+    void print(){
+        System.out.print(" ");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print(" "+i);
+        }
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print("\n"+i+" ");
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j]+" ");
+            }
+        }
+    }
+}
+
+class DijsktraALGO {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        DIJ x=new DIJ();
+        System.out.println("Want to insert");
+        char ans=in.next().charAt(0);
+        while(ans=='Y' || ans=='y'){
+            x.insert(in.nextInt(), in.nextInt(), in.nextInt());
+            System.out.println("More....?");
+            ans=in.next().charAt(0);
+        }
+        x.print();
+        x.dijskstra(x.arr.length,in.nextInt());
+        in.close();
+    }
+}
