11369e5c61f3c51d8e6387f50b14f53a1fe5bae3
Kanishq <79330066+Kanishq10@users.noreply.github.com>
2022-02-26 23:43:02 +0530

    commit

--- java.files/advance__java/ExcepTION.java
+++ java.files/advance__java/ExcepTION.java
@@ -1,1 +1,23 @@
-class ExcepTION{   static int i1, i2, i3;   public static void main(String[] args)   {      try      {         test(i1 = 7, oops(i2=2), i3 = 10);      } catch (Exception e)      {         System.out.println(i1+" "+i2+" "+i3);      }   }   static int oops(int i) throws Exception   {      throw new Exception("oops");   }   static int test(int a, int b, int c) {         return a + b + c;      }}
+package advance__java;
+
+class ExcepTION
+{
+   static int i1, i2, i3;
+   public static void main(String[] args)
+   {
+      try
+      {
+         test(i1 = 7, oops(i2=2), i3 = 10);
+      } catch (Exception e)
+      {
+         System.out.println(i1+" "+i2+" "+i3);
+      }
+   }
+   static int oops(int i) throws Exception
+   {
+      throw new Exception("oops");
+   }
+   static int test(int a, int b, int c) {   
+      return a + b + c;   
+   }
+}
--- java.files/fabinoccino.java
+++ java.files/fabinoccino.java
@@ -1,5 +1,19 @@
  class Fibonacci {
-
+    public static int fb(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int x=fb(n-1)+fb(n-2);
+        return x;
+    }
+    static int qb[]=new int[50];
+    public static int fbmemoised(int n){    //Dynamic programmed :memoised
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(qb[n]!=0) return qb[n];
+        qb[n]=fb(n-1)+fb(n-2);
+        return qb[n];
+    }
+    
     public static void main(String args[]){
 
         int n = 11, a = 0, b = 1;
@@ -12,5 +26,8 @@
             a = b;
             b = sum;
         }
+
+        System.out.println("\nNth element is in Fabonacci Series is "+fb(48));
+        System.out.println("\nNth element is in Fabonacci Series is "+fb(48));
     }
 }
